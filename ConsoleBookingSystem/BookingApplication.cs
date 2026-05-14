8f31c8d7e910246f559ddf16a3bd38945bfc087f
evgen-web-dev <217228725+evgen-web-dev@users.noreply.github.com>
2026-05-14 23:30:00 +0300

    refactor: re-organize code in BookingApplication

--- ConsoleBookingSystem/BookingApplication.cs
+++ ConsoleBookingSystem/BookingApplication.cs
@@ -14,195 +14,17 @@ public class BookingApplication
         { BookingApplicationActionType.ExitApplication, "Exit the application" },
     };
     
-    
     public BookingApplication(IHostService hostService, IConsoleInputOutput io)
     {
         _hostsService = hostService;
         _io = io;
     }
-
-
-    int TryRetrieveHostId(string message, string invalidInputMessage)
-    {
-        int hostIdToRetrieve;
         
-        // iterating until user inputs valid and existing host-id
-        while (true)
-        {
-            hostIdToRetrieve = _io.ReadInt(message, true, invalidInputMessage);
-            if (_hostsService.HostExistsById(hostIdToRetrieve))
-            {
-                break;
-            }
-            _io.Write($"Host with ID = {hostIdToRetrieve} does not exist. Please try again.");
-        }
-
-        return hostIdToRetrieve;
-    }
-    
-    
-    void HandleShowAllHostsAction()
+    public void LaunchApplication()
     {
-        _io.Write("\n===== List of all Hosts =====\n");
-        
-        var hosts = _hostsService.GetHosts();
-
-        if (hosts.Count == 0)
-        {
-            _io.Write("No Hosts added yet.");
-            return;
-        }
-        
-        foreach (var host in hosts)
-        {
-            _io.Write(host.ToString());
-        }
+        _io.Write("\n============= Hello, this is Booking System app! =============\n");
+        ShowActionsMenu();
     }
-    
-    
-    void HandleShowHostDetailsAction()
-    {
-        var searchedHostId = TryRetrieveHostId(
-            "\nEnter ID of the Host you are searching for (confirm input by pressing Enter):",
-            "\nInvalid input - Host ID must be an integer. Please try again."
-        );
-    
-        var host = _hostsService.GetHostById(searchedHostId);
-        if (host is null) 
-        {
-            _io.Write($"\nNo host with found with ID = {searchedHostId}.");
-            return;
-        }
-
-        _io.Write("\n==== Search results: ====\n");
-        _io.Write(host.ToString());
-    
-        if (host.Apartments.Count == 0)
-        {
-            _io.Write("No apartments found for current Host.");
-            return;
-        }
-
-        foreach (var apartment in host.Apartments)
-        {
-            _io.Write($" - {apartment}");
-        }
-    }
-
-
-    void HandleAddNewHostAction()
-    {
-        _io.Write("\n==== You are adding new Host: ====\n");
-        
-        string newHostName = _io.ReadString("Enter new Host name:", true, "Host name can not be empty, please try again.").Trim();
-        string newHostAddress = _io.ReadString("Enter new Host address:", true, "Host address can not be empty, please try again.").Trim();
-        
-        try
-        {
-            _hostsService.AddHost(newHostName, newHostAddress);
-            // this line is expected to be shown when Host removed successfully
-            _io.Write("\nHost added successfully!");
-        }
-        catch (Exception exception)
-        {
-            // this line is expected to be shown when there was an error during adding a Host
-            // (Host with specified name and address already exists, etc)
-            _io.Write($"\n{exception.Message}");
-        }
-    }
-
-
-    void HandleRemoveHostAction()
-    {
-        _io.Write("\n==== You are removing a Host: ====");
-        
-        var hostIdToRemove = TryRetrieveHostId(
-            "\nEnter ID of the Host that needs to be removed (confirm input by pressing Enter):",
-            "\nInvalid host ID entered, please try again."
-        );
-        try
-        {
-            _hostsService.RemoveHostById(hostIdToRemove);
-            // this line is expected to be shown when Host removed successfully
-            _io.Write("\nHost removed successfully!");
-        }
-        catch (Exception exception)
-        {
-            // this line is expected to be shown when there was an error during deleting a Host
-            // (Host with specified ID was not found, etc)
-            _io.Write($"\n{exception.Message}");
-        }
-    }
-    
-    
-    void HandleUpdateHostAction()
-    {
-        _io.Write("\n==== You are updating a particular Host's data ====");
-        
-        try
-        {
-            int hostIdToUpdate = TryRetrieveHostId(
-                "\nEnter ID of the Host that needs to be updated (confirm input by pressing Enter):",
-                "\nInvalid host ID entered, please try again.");
-            
-            var newHostName = _io.ReadString("Enter updated Host name:", true, "Host name can not be empty, please try again.").Trim();
-            var newHostAddress = _io.ReadString("Enter updated Host address:", true, "Host address can not be empty, please try again.").Trim();
-            
-            _hostsService.EditHostById(hostIdToUpdate, newHostName, newHostAddress);
-            
-            // this line is expected to be shown when Host removed successfully
-            _io.Write("\nHost is updated successfully!");
-        }
-        catch (Exception exception)
-        {
-            // this line is expected to be shown when there was an error during deleting a Host
-            // (Host with specified ID was not found, etc)
-            _io.Write($"\n{exception.Message}");
-        }
-    }
-    
-    
-    void HandleExitAppAction()
-    {
-        _io.Write("\nExiting... See you next time!");
-    }
-    
-    
-    void HandleInvalidActionInput()
-    {
-        _io.Write("\nInvalid number: a number for an action must be between 0 and 2.");
-    }
-
-
-    ProcessedBookingApplicationActionResult ProcessAction()
-    {
-        BookingApplicationActionType currentActionType;
-            
-        currentActionType = _io.ReadEnumValue<BookingApplicationActionType>("", true, "Invalid action selected selected, please try again.");
-
-        switch (currentActionType)
-        {
-            case BookingApplicationActionType.ShowAllHosts: HandleShowAllHostsAction(); break;
-            case BookingApplicationActionType.ShowHostDetails: HandleShowHostDetailsAction(); break;
-            case BookingApplicationActionType.AddNewHost: HandleAddNewHostAction(); break;
-            case BookingApplicationActionType.RemoveHost: HandleRemoveHostAction(); break;
-            case BookingApplicationActionType.UpdateHost: HandleUpdateHostAction(); break;
-            case BookingApplicationActionType.ExitApplication:
-            {
-                HandleExitAppAction();
-                return ProcessedBookingApplicationActionResult.GotExitApplicationRequest;
-            }
-            default:
-            {
-                HandleInvalidActionInput();
-                // "continue;" keyword is for new iteration of while loop, related to switch {}
-                return ProcessedBookingApplicationActionResult.GotInvalidAction;
-            }
-        }
-        
-        return ProcessedBookingApplicationActionResult.GotAndProcessedValidAction;
-    }
-
 
     void ShowActionsMenu()
     {
@@ -240,11 +62,176 @@ public class BookingApplication
         }
     }
 
-
-    public void LaunchApplication()
+    ProcessedBookingApplicationActionResult ProcessAction()
     {
-        _io.Write("\n============= Hello, this is Booking System app! =============\n");
-        ShowActionsMenu();
+        BookingApplicationActionType currentActionType;
+            
+        currentActionType = _io.ReadEnumValue<BookingApplicationActionType>("", true, "Invalid action selected selected, please try again.");
+
+        switch (currentActionType)
+        {
+            case BookingApplicationActionType.ShowAllHosts: HandleShowAllHostsAction(); break;
+            case BookingApplicationActionType.ShowHostDetails: HandleShowHostDetailsAction(); break;
+            case BookingApplicationActionType.AddNewHost: HandleAddNewHostAction(); break;
+            case BookingApplicationActionType.RemoveHost: HandleRemoveHostAction(); break;
+            case BookingApplicationActionType.UpdateHost: HandleUpdateHostAction(); break;
+            case BookingApplicationActionType.ExitApplication:
+            {
+                HandleExitAppAction();
+                return ProcessedBookingApplicationActionResult.GotExitApplicationRequest;
+            }
+            default:
+            {
+                HandleInvalidActionInput();
+                // "continue;" keyword is for new iteration of while loop, and is not related to switch {}
+                return ProcessedBookingApplicationActionResult.GotInvalidAction;
+            }
+        }
+        
+        return ProcessedBookingApplicationActionResult.GotAndProcessedValidAction;
     }
 
+    private int TryRetrieveHostId(string message, string invalidInputMessage)
+    {
+        int hostIdToRetrieve;
+        
+        // iterating until user inputs valid and existing host-id
+        while (true)
+        {
+            hostIdToRetrieve = _io.ReadInt(message, true, invalidInputMessage);
+            if (_hostsService.HostExistsById(hostIdToRetrieve))
+            {
+                break;
+            }
+            _io.Write($"Host with ID = {hostIdToRetrieve} does not exist. Please try again.");
+        }
+
+        return hostIdToRetrieve;
+    }
+    
+    void HandleShowAllHostsAction()
+    {
+        _io.Write("\n===== List of all Hosts =====\n");
+        
+        var hosts = _hostsService.GetHosts();
+
+        if (hosts.Count == 0)
+        {
+            _io.Write("No Hosts added yet.");
+            return;
+        }
+        
+        foreach (var host in hosts)
+        {
+            _io.Write(host.ToString());
+        }
+    }
+    
+    void HandleShowHostDetailsAction()
+    {
+        var searchedHostId = TryRetrieveHostId(
+            "\nEnter ID of the Host you are searching for (confirm input by pressing Enter):",
+            "\nInvalid input - Host ID must be an integer. Please try again."
+        );
+    
+        var host = _hostsService.GetHostById(searchedHostId);
+        if (host is null) 
+        {
+            _io.Write($"\nNo host with found with ID = {searchedHostId}.");
+            return;
+        }
+
+        _io.Write("\n==== Search results: ====\n");
+        _io.Write(host.ToString());
+    
+        if (host.Apartments.Count == 0)
+        {
+            _io.Write("No apartments found for current Host.");
+            return;
+        }
+
+        foreach (var apartment in host.Apartments)
+        {
+            _io.Write($" - {apartment}");
+        }
+    }
+
+    void HandleAddNewHostAction()
+    {
+        _io.Write("\n==== You are adding new Host: ====\n");
+        
+        string newHostName = _io.ReadString("Enter new Host name:", true, "Host name can not be empty, please try again.").Trim();
+        string newHostAddress = _io.ReadString("Enter new Host address:", true, "Host address can not be empty, please try again.").Trim();
+        
+        try
+        {
+            _hostsService.AddHost(newHostName, newHostAddress);
+            // this line is expected to be shown when Host removed successfully
+            _io.Write("\nHost added successfully!");
+        }
+        catch (Exception exception)
+        {
+            // this line is expected to be shown when there was an error during adding a Host
+            // (Host with specified name and address already exists, etc)
+            _io.Write($"\n{exception.Message}");
+        }
+    }
+
+    void HandleRemoveHostAction()
+    {
+        _io.Write("\n==== You are removing a Host: ====");
+        
+        var hostIdToRemove = TryRetrieveHostId(
+            "\nEnter ID of the Host that needs to be removed (confirm input by pressing Enter):",
+            "\nInvalid host ID entered, please try again."
+        );
+        try
+        {
+            _hostsService.RemoveHostById(hostIdToRemove);
+            // this line is expected to be shown when Host removed successfully
+            _io.Write("\nHost removed successfully!");
+        }
+        catch (Exception exception)
+        {
+            // this line is expected to be shown when there was an error during deleting a Host
+            // (Host with specified ID was not found, etc)
+            _io.Write($"\n{exception.Message}");
+        }
+    }
+    
+    void HandleUpdateHostAction()
+    {
+        _io.Write("\n==== You are updating a particular Host's data ====");
+        
+        try
+        {
+            int hostIdToUpdate = TryRetrieveHostId(
+                "\nEnter ID of the Host that needs to be updated (confirm input by pressing Enter):",
+                "\nInvalid host ID entered, please try again.");
+            
+            var newHostName = _io.ReadString("Enter updated Host name:", true, "Host name can not be empty, please try again.").Trim();
+            var newHostAddress = _io.ReadString("Enter updated Host address:", true, "Host address can not be empty, please try again.").Trim();
+            
+            _hostsService.EditHostById(hostIdToUpdate, newHostName, newHostAddress);
+            
+            // this line is expected to be shown when Host removed successfully
+            _io.Write("\nHost is updated successfully!");
+        }
+        catch (Exception exception)
+        {
+            // this line is expected to be shown when there was an error during deleting a Host
+            // (Host with specified ID was not found, etc)
+            _io.Write($"\n{exception.Message}");
+        }
+    }
+    
+    void HandleExitAppAction()
+    {
+        _io.Write("\nExiting... See you next time!");
+    }
+    
+    void HandleInvalidActionInput()
+    {
+        _io.Write("\nInvalid number: a number for an action must be between 0 and 2.");
+    }
 }
