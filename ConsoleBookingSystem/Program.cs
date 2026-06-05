df1467f91a7e5a6dc1ddcfc1e5f657549fd07a5c
evgen-web-dev <217228725+evgen-web-dev@users.noreply.github.com>
2026-06-05 18:29:08 +0300

    test: comment test code
    - comment test code that simulated race condition
    - uncomment "normal app flow" code

--- ConsoleBookingSystem/Program.cs
+++ ConsoleBookingSystem/Program.cs
@@ -1,38 +1,36 @@
 ﻿using ConsoleBookingSystem;
 
 
-// initializing needed dependencies
-// var io = new ConsoleInputOutput();
-//
-// var jsonFilePersistence = new JsonFilePersistence<List<Host>>("hosts.json");
-// IHostRepository hostRepository;
-//
-// try
-// {
-//     hostRepository = new JsonFileHostRepository(jsonFilePersistence);
-// }
-// catch (PersistenceException exception)
-// {
-//     io.Write("\nSomething went wrong during in initializing data storage:");
-//     io.Write(exception.Message);
-//     return;
-// }
-//
-// var hostService = new HostService(hostRepository);
-//
-// // creating booking app instance with passing needed dependencies
-// var bookingApplication = new BookingApplication(hostService, io);
-// bookingApplication.LaunchApplication();
+var io = new ConsoleInputOutput();
 
+var jsonFilePersistence = new JsonFilePersistence<List<Host>>("hosts.json");
+IHostRepository hostRepository;
 
-
-// ======= Race condition simulation for Hosts & Apartments (confirmed at June 4, 2026, 13:00 UTC and will be (or already is) fixed)
-var raceConditionRepository = new InMemoryHostRepository((new RaceConditionSimulatorHostSeeder()).GetHosts());
-var raceConditionSimulator = new HostRaceConditionSimulator(raceConditionRepository);
-
-for (int i = 0; i < 50; i++)
+try
 {
-    await raceConditionSimulator.IncreasePriceForSharedApartment(5);
+    hostRepository = new JsonFileHostRepository(jsonFilePersistence);
 }
-    
-// ======= Race condition simulation for Hosts & Apartments (confirmed at June 4, 2026, 13:00 UTC and will be (or already is) fixed)
+catch (PersistenceException exception)
+{
+    io.Write("\nSomething went wrong during in initializing data storage:");
+    io.Write(exception.Message);
+    return;
+}
+
+var hostService = new HostService(hostRepository);
+
+var bookingApplication = new BookingApplication(hostService, io);
+bookingApplication.LaunchApplication();
+
+
+
+// // ======= Race condition simulation for Hosts & Apartments (confirmed at June 4, 2026, 13:00 UTC and fixed in 71dceaf2f6fcc250abec3eb3158430880cfae957 commit)
+// var raceConditionRepository = new InMemoryHostRepository((new RaceConditionSimulatorHostSeeder()).GetHosts());
+// var raceConditionSimulator = new HostRaceConditionSimulator(raceConditionRepository);
+//
+// for (int i = 0; i < 50; i++)
+// {
+//     await raceConditionSimulator.IncreasePriceForSharedApartment(5);
+// }
+//     
+// // ======= Race condition simulation for Hosts & Apartments (confirmed at June 4, 2026, 13:00 UTC and fixed in 71dceaf2f6fcc250abec3eb3158430880cfae957 commit)
